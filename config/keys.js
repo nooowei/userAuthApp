92de532b51a01964cfd8f5972af5f70cfdb18d0d
unknown <wei.xw.xu@gmail.com>
2020-01-02 10:26:05 -0500

    bug fix

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: "mongodb+srv://wei:615934236@fitnesstrackerdb-2fcbg.gcp.mongodb.net/test?retryWrites=true&w=majority"
+}
---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// schema name, schema, collection
+const User = mongoose.model('User', UserSchema, 'userAuth');
+
+module.exports = User;
